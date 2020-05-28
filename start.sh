fa6fd7a018884a5dd1158ac2d3d89c91260c0afd
wenying.wu <wenying.wu@baisonmai.com>
2020-05-28 18:28:25 +0800

    1

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /usr/local/nsq-1.2.0.linux-amd64.go1.12.9/bin
 nohup ./nsqlookupd &
-nohup ./nsqd --broadcast-address=150.158.218.222 --lookupd-tcp-address=150.158.218.222:4160 --msg-timeout=900m0s  &
+nohup ./nsqd --lookupd-tcp-address=127.0.0.1:4160  &
 nohup ./nsqadmin --lookupd-http-address=127.0.0.1:4161 &
 while :
 do
